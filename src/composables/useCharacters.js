1718585823b6094b68f807c1f664e36ea8ea3300
ashle <amireles1334@yahoo.com>
2023-03-04 01:53:14 -0600

    fetch individual characters

--- src/composables/useCharacters.js
+++ src/composables/useCharacters.js
@@ -2,20 +2,28 @@ import { ref } from 'vue'
 import useApi from '@/composables/useApi'
 
 const characters = ref([])
+const currentCharacter = ref({})
+
 const api = useApi()
 const page = ref(1)
 
 const useCharacters = () => {
-    const fetchCharacters = async () => {
-        const { data } = await api.instance.get('/characters', {
-            params: {
-                page: page.value,
-            }
-        })
-        characters.value.push(...data.data)
-        page.value++
-    }
-    return {characters, fetchCharacters}
+  const fetchCharacters = async () => {
+    const { data } = await api.instance.get('/characters', {
+      params: {
+        page: page.value,
+      },
+    })
+    characters.value.push(...data.data)
+    page.value++
+  }
+
+  const fetchCharacter = async (id) => {
+    const { data } = await api.instance.get(`/characters/${id}`)
+    currentCharacter.value = data
+  }
+
+  return { characters, fetchCharacters, fetchCharacter, currentCharacter }
 }
 
 export default useCharacters
